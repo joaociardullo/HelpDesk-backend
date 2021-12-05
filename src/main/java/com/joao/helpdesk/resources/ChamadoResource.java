b4bdf86067f3b3ce9790e233af0fca1469a0942e
joaociardullo <joaociardullo@hotmail.com>
2021-12-05 16:59:12 -0300

    FindAll para chamados

--- src/main/java/com/joao/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/joao/helpdesk/resources/ChamadoResource.java
@@ -1,5 +1,8 @@
 package com.joao.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,5 +26,12 @@ public class ChamadoResource {
 		Chamado obj = service.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDTO(obj));
 
+	}
+//Listagem de todos os chamdos do banco 
+	@GetMapping
+	public ResponseEntity<List<ChamadoDTO>> findAll() {
+		List<Chamado> list = service.findAll();
+		List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
--- src/main/java/com/joao/helpdesk/services/ChamadoService.java
+++ src/main/java/com/joao/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.joao.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +21,12 @@ public class ChamadoService {
 		Optional<Chamado> obj =repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encotrado" + id));
 	}
+
+
+	public List<Chamado> findAll() {
+
+		return repository.findAll();
+	}
+	
+	
 }
